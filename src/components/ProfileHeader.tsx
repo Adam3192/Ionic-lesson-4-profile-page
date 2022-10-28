f1c7cc4d435d8fc92e8b2ef142fe92af7a1f3c10
Adam3192 <jeremyadixon@hotmail.com>
2022-10-28 15:19:01 -0500

    profile page

--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -4,8 +4,14 @@ import {
   IonBreadcrumb,
   IonBreadcrumbs,
   IonButtons,
+  IonCol,
+  IonGrid,
   IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
   IonMenuButton,
+  IonRow,
   IonThumbnail,
   IonTitle,
   IonToolbar,
@@ -32,12 +38,41 @@ const ProfileHeader: React.FC<ContainerProps> = () => {
             <p>Full Stack Developer</p>
           </div>
         </div>
-      <IonBreadcrumbs>
-        <IonBreadcrumb href="/profile/bio">Bio</IonBreadcrumb>
-        <IonBreadcrumb href="/profile/posts">Posts</IonBreadcrumb>
-        <IonBreadcrumb href="/profile/friends">Friends</IonBreadcrumb>
-        <IonBreadcrumb active={false} href="/profile/photos">Photos</IonBreadcrumb>
-      </IonBreadcrumbs>
+
+
+        <IonGrid>
+          <IonRow>
+            <IonItem lines="none" detail={false} routerLink='/profile/bio'>
+               <IonLabel>Bio</IonLabel>
+            </IonItem>
+            <IonItem lines="none" detail={false} routerLink='/profile/posts'>
+               <IonLabel>Posts</IonLabel>
+            </IonItem>
+            <IonItem lines="none" detail={false} routerLink='/profile/friends'>
+               <IonLabel>Friends</IonLabel>
+            </IonItem>
+            <IonItem lines="none" detail={false} routerLink='/profile/photos'>
+               <IonLabel>Photos</IonLabel>
+            </IonItem>
+          </IonRow>
+        </IonGrid>
+
+
+        {/* <IonBreadcrumbs className="ion-justify-content-center ion-padding-top">
+          <IonBreadcrumb separator={false} href="/profile/bio">
+            Bio
+          </IonBreadcrumb>
+          <IonBreadcrumb separator={false} href="/profile/posts">
+            Posts
+          </IonBreadcrumb>
+          <IonBreadcrumb separator={false} href="/profile/friends">
+            Friends
+          </IonBreadcrumb>
+          <IonBreadcrumb active={false} href="/profile/photos">
+            Photos
+          </IonBreadcrumb>
+        </IonBreadcrumbs> */}
+
       </IonHeader>
     </div>
   )
